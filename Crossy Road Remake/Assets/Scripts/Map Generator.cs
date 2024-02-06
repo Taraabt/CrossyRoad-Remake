5ce3a8485163896232271edeba86f76748a297d4
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-02-06 11:15:25 +0100

    sistemation map creation and add obstaccle

--- Crossy Road Remake/Assets/Scripts/Map Generator.cs
+++ Crossy Road Remake/Assets/Scripts/Map Generator.cs
@@ -1,30 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour{
-    [SerializeField] GameObject[] map;
 
-    int minRange = 0;
-    int maxRange = 4;
-    float maxPos=10f;
-    int maxUnit=5;
+    [SerializeField] GameObject[] ground;
+    [SerializeField] GameObject[] obstacle;
+    float maxPos =10f;
+    float startingLine=5f;
+    int maxObstacle = 3;
+    List<int> List = new List<int>();
 
-    public void CreateNewLine(float i){
-        int r = Random.Range(minRange, maxRange);
-        Vector3 newLine = new Vector3(0f, 0f, map[r].transform.position.z + i);
-        Instantiate(map[r], newLine, Quaternion.identity);
-    }
-    void Start(){
-        for (int i=maxUnit ; i < maxPos; i++) {
-            CreateNewLine(i);
+    public void CreateLine(float i){
+        int r = Random.Range(0, 4);
+        Vector3 newLine = new Vector3(-0.5f, 0f, ground[r].transform.position.z + i);
+        GameObject Instance=Instantiate(ground[r], newLine, Quaternion.identity);
+        if (r == 0){
+            CreateGroundObstacle(Instance.transform.position);
         }
     }
+
+    public void CreateGroundObstacle(Vector3 position){
+        for (int i = -5; i < 5; i++){
+            List.Add(i);
+        }
+        for (int i = 0;i < maxObstacle; i++){
+            int r = Random.Range(0, List.Count);
+            Vector3 pos = new Vector3(List[r], 0f, position.z);
+            List.Remove(r-5);
+            for (int j =0; j < List.Count; j++){
+                Debug.Log(List[j]);
+            }
+            Instantiate(obstacle[Random.Range(0, 5)], pos, Quaternion.identity);
+        }
+        List.Clear();
+    }
+
+    void Start(){
+        for (float i=startingLine ; i < maxPos; i++) {
+            CreateLine(i);
+        }
+    }
+
     void Update(){
         GameObject player = GameObject.Find("Chicken");
-        if(player.transform.position.z +maxUnit >= maxPos){
-            CreateNewLine(maxPos);
+        if(player.transform.position.z +startingLine >= maxPos){
+            CreateLine(maxPos);
             maxPos++;
         }
     }
+
+
 }
