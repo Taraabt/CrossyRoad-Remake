bda1090a954f5ca2a4887f07df645a7cdec1d6da
Alberto Tarabella <alberto.tarabella04@gmail.com>
2024-02-20 16:03:26 +0100

    merging

---
+++ Crossy Road Remake/Assets/Scripts/Destroy Object.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour{
+
+    void Update(){
+        GameObject player = GameObject.Find("Chicken");
+        Vector3 playerPos = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z-12);
+        Vector3 boxDimension = new Vector3(11f,3f,1);
+        Collider[] toDestroy = Physics.OverlapBox(playerPos, boxDimension);
+        for (int i = 0; i < toDestroy.Length; i++){
+            //Destroy(toDestroy[i]);
+            Destroy(toDestroy[i].gameObject);
+        }
+    }
+}
